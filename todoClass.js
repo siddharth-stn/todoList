8b2cf15551cabeeeb3e4e5910255605d45aaf623
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-02-28 11:59:20 +0530

    First Commit

---
+++ todoClass.js
@@ -0,0 +1,10 @@
+
+
+class ToDoMaker {
+    constructor(title, description, dueDate, priority) {
+        this._title = title;
+        this._description = description;
+        this._dueDate = dueDate;
+        this._priority = priority;
+    };
+}; 
