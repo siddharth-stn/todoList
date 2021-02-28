b1fb6f02ec9c506af84f17c34051133d342af03b
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-02-28 12:17:44 +0530

    logic for the app is alive requires improvements

--- todoClass.js
+++ todoClass.js
@@ -1,6 +1,6 @@
 
 
-class ToDoMaker {
+class ToDo {
     constructor(title, description, dueDate, priority) {
         this._title = title;
         this._description = description;
@@ -8,3 +8,21 @@ class ToDoMaker {
         this._priority = priority;
     };
 }; 
+
+class Project {
+    constructor (projName, description) {
+        this._projName = projName;
+        this._description = description;
+    };
+
+    createTodos (title, description, dueDate, priority) {
+        return new ToDo (title, description, dueDate, priority)
+    };
+};
+
+let proj1 = new Project("Project_One", "This is a test project");
+let todo1 = proj1.createTodos("test-todo", 'this is a test todo description', 
+"28/February/2021", "Low");
+
+console.log(proj1);
+console.log(todo1);
