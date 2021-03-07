2c5705825f48a5592d7d88bd23395c57da7004e9
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-03-07 16:15:17 +0530

    eslint done on working.js and todoClass.js

--- todoClass.js
+++ todoClass.js
@@ -1,25 +1,24 @@
 
-
 class ToDo {
-    constructor(title, description, dueDate, priority, relation) {
-        this._title = title;
-        this._description = description;
-        this._dueDate = dueDate;
-        this._priority = priority;
-        this._relation = relation;
-    };
-}; 
-
-class Project {
-    constructor (projName, description) {
-        this._projName = projName;
-        this._description = description;
-        this._toDoList = []
-    };
-
-    createTodos (title, description, dueDate, priority, relation = this._projName) {
-        this._toDoList.push(new ToDo (title, description, dueDate, priority, relation)); 
-    };
+  constructor (title, description, dueDate, priority, relation) {
+    this._title = title
+    this._description = description
+    this._dueDate = dueDate
+    this._priority = priority
+    this._relation = relation
+  };
 };
 
-export default Project;
+class Project {
+  constructor (projName, description) {
+    this._projName = projName
+    this._description = description
+    this._toDoList = []
+  };
+
+  createTodos (title, description, dueDate, priority, relation = this._projName) {
+    this._toDoList.push(new ToDo(title, description, dueDate, priority, relation))
+  };
+};
+
+export default Project
